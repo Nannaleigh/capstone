da7fe77764c97ff2438bd40b25401cd2467ced92
Nannaleigh <tatianna.martinez71@gmail.com>
2022-02-22 12:41:10 -0700

    minor changes and clean up

--- server/server.js
+++ server/server.js
@@ -4,16 +4,16 @@ const app = express()
 const path = require('path')
 const {SERVER_PORT} = process.env || 5464
 const { seed } = require('./seed.js')
-const { getTodo } = require('./controller.js')
+
 app.use(express.json())
 app.use(express.static('public'))
 
 app.post('/seed', seed)
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/index.html'))
-// });
-app.get('/todo', getTodo)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
 
 app.listen(SERVER_PORT, () => console.log (`Listening to you on port ${SERVER_PORT}`))
 
